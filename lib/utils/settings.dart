47f68fa54d95978f636e7c309d056bd3ba8ac74e
Milajii <mariamiladevz@gmail.com>
2024-03-18 21:05:22 -0400

    token que funciono

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,5 +1,5 @@
 class Settings {
   static const String appId = "f3c07136dd044819a9843fdabc4c0544";
   static const String token =
-      "007eJxTYEhdXltdzP9y+b3mIoG81+aFnAs53xfdjX5+jN/2U43n/2cKDGnGyQbmhsZmKSkGJiYWhpaJlhYmxmkpiUnJJskGpiYm+jMWpDYEMjIIGLawMjJAIIjPxZCWU1pSklqUWFDAwAAAUEoiNw==";
+      "007eJxTYFh1NfGyoUVBwXaRaXm70xPLVhf8PeIod8XT842ohof3nl4FhjTjZANzQ2OzlBQDExMLQ8tESwsT47SUxKRkk2QDUxOTiLU/UgX4GBhCvDWZGRkYGViAGASYwCQzmGQBk/wMxYk5iQoFicmZqXklqQqGjAyGAIyeIS4=";
 }
